af7c17b37e600dfceecf0bd3f4872f0a30e13156
Safikul Islam Sakib <92842634+sakibulcode@users.noreply.github.com>
2021-10-20 17:04:31 +0600

    Add files via upload

---
+++ URI Online Judge/URI1001.java
@@ -0,0 +1,20 @@
+package ProblemSolve;
+import java.util.Scanner;
+
+public class URI1001 {
+
+	public static void main(String[] args) {
+		int A, B, X;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		A = scan.nextInt();
+		B = scan.nextInt();
+		
+		X = A + B;
+		
+		System.out.println("X = " +X);
+
+	}
+
+}
---
+++ URI Online Judge/URI1004.java
@@ -0,0 +1,20 @@
+package ProblemSolve;
+import java.util.Scanner;
+
+public class URI1004 {
+
+	public static void main(String[] args) {
+		int a, b, PROD;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		a = scan.nextInt();
+		b = scan.nextInt();
+		
+		PROD = a * b;
+		
+		System.out.println("PROD = " +PROD);
+
+	}
+
+}
