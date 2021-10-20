3a130d81778ad7f73a024564ee916b473f0c17b7
Safikul Islam Sakib <92842634+sakibulcode@users.noreply.github.com>
2021-10-20 16:45:00 +0600

    Add files via upload

---
+++ URI1003.java
@@ -0,0 +1,20 @@
+package ProblemSolve;
+import java.util.Scanner;
+
+public class URI1003 {
+
+	public static void main(String[] args) {
+		int A, B, SOMA;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		A = scan.nextInt();
+		B = scan.nextInt();
+		
+		SOMA = A + B;
+		
+		System.out.println("SOMA = " +SOMA);
+
+	}
+
+}
