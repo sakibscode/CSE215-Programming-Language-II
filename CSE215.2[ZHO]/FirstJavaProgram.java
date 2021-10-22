1fcffc50a461eaa59943d92d0d81829efc8d48d1
Safikul Islam Sakib <92842634+sakibulcode@users.noreply.github.com>
2021-10-22 12:45:38 +0600

    Add files via upload

---
+++ CSE215.2[ZHO]/FirstJavaProgram.java
@@ -0,0 +1,10 @@
+package Lec1;
+
+public class FirstJavaProgram {
+
+	public static void main(String[] args) {
+		System.out.println("This is a simple Java program.");
+
+	}
+
+}
---
+++ CSE215.2[ZHO]/SecondJavaProgram.java
@@ -0,0 +1,17 @@
+package Lec1;
+
+public class SecondJavaProgram {
+
+	public static void main(String[] args) {
+		int num;
+		num = 100;
+		
+		System.out.println("This is a number : " +num);
+		
+		num = num*2;
+		
+		System.out.println("The value of num*2 is = " +num);
+
+	}
+
+}
