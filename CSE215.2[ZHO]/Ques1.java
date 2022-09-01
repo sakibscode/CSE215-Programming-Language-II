ebc8a77e4e4987c535d2acf74a2e9050c8a81cfc
Safikul Islam Sakib <92842634+sakibscode@users.noreply.github.com>
2022-09-01 13:50:20 +0600

    Add files via upload

---
+++ CSE215.2[ZHO]/Ques1.java
@@ -0,0 +1,10 @@
+package Lec5;
+
+public class Ques1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ CSE215.2[ZHO]/Xm1.java
@@ -0,0 +1,12 @@
+package Lec5;
+
+public class Xm1 {
+
+	public static void main(String[] args) {
+		for(int i=0; i<10; i++) {
+			System.out.println("Safikul Islam Sakib");
+		}
+
+	}
+
+}
