2d8ad410a2ec48897b5c39ce31ee0638c50f2728
Safikul Islam Sakib <92842634+sakibscode@users.noreply.github.com>
2022-09-01 13:47:30 +0600

    Add files via upload

---
+++ CSE215.2[ZHO]/ForExample.java
@@ -0,0 +1,10 @@
+package Lec2;
+
+public class ForExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ CSE215.2[ZHO]/IfExample.java
@@ -0,0 +1,26 @@
+package Lec2;
+
+public class IfExample {
+
+	public static void main(String[] args) {
+		int x, y;
+		x = 10;
+		y = 20;
+		
+		if(x < y) {
+			System.out.println("x is less than y.");
+		}
+		
+		x = x*2;
+		
+		if(x == y) {
+			System.out.println("x now equal to y.");
+		}
+		
+		if(x == y) {
+			System.out.println("you won't see this.");
+		}
+
+	}
+
+}
